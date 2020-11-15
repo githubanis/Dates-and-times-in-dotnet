6248159dd6ab9a6811c009724c094c1c088c5eeb
githubanis <anissarker301@gmail.com>
2020-11-15 12:15:47 +0600

    Add project files.

---
+++ Dates-and-times-in-dotnet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dates_and_times_in_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now;
+
+            TimeZoneInfo sydneyTimeZone =
+                TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
+
+        }
+    }
+}
